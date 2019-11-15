441b9f696e3b2777378d8f23779db2388c2d111e
Larissa Espelocin <larissa.justen@cwi.com.br>
2019-11-15 16:01:21 -0300

    manter

--- app/src/tela/cadastroProva/cadastroProva.tela.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import './cadastroProva.style.css'
-
-export class CadastroProvaScreen extends Component {
-
-    render() {
-        return <h1>Oi</h1>
-    }
-}
