64cc4cdbbebb55d63b478131e670076d7b49c305
Antonio Rosa <antonio.rosa@cwi.com.br>
2019-11-16 22:24:57 -0300

    Testes novas services feitos

--- api/src/test/java/br/com/cwi/crescer/api/services/usuario/AdicionarNovoUsuarioServiceTest.java
+++ api/src/test/java/br/com/cwi/crescer/api/services/usuario/AdicionarNovoUsuarioServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AdicionarNovoUsuarioServiceTest {
@@ -20,17 +21,51 @@ public class AdicionarNovoUsuarioServiceTest {
     @Mock
     UsuarioRepository repository;
 
+    @Mock
+    ModelMapper mapper;
+
+    @Test
+    public void deveChamarModelMapperQuandoAdicionarNovoUsuarioServiceForChamado() {
+
+        UsuarioRequest request = new UsuarioRequest();
+
+        Usuario usuario = new Usuario();
+
+        Mockito.when(mapper.map(request, Usuario.class)).thenReturn(usuario);
+        Mockito.when(repository.save(usuario)).thenReturn(usuario);
+
+        adicionarNovoUsuarioService.adicionar(request);
+
+        Mockito.verify(mapper).map(request, Usuario.class);
+    }
+
     @Test
     public void deveChamarUsuarioRepositoryQuandoAdicionarNovoUsuarioServiceForChamado() {
-//        UsuarioRequest request = new UsuarioRequest();
-//        Usuario usuarioMapeado = new Usuario();
-//        Usuario usuarioSalvo = new Usuario();
-//
-//        Mockito.when(repository.save(usuarioMapeado)).thenReturn(usuarioSalvo);
-//
-//        Usuario usuarioResultado = adicionarNovoUsuarioService.adicionar(request);
-//
-//        Assert.assertSame(usuarioResultado, usuarioSalvo);
 
+        UsuarioRequest request = new UsuarioRequest();
+
+        Usuario usuario = new Usuario();
+
+        Mockito.when(mapper.map(request, Usuario.class)).thenReturn(usuario);
+        Mockito.when(repository.save(usuario)).thenReturn(usuario);
+
+        adicionarNovoUsuarioService.adicionar(request);
+
+        Mockito.verify(repository).save(usuario);
+    }
+
+    @Test
+    public void deveRetornarUmUsuarioQuandoAdicionarNovoUsuarioServiceForChamado() {
+
+        UsuarioRequest request = new UsuarioRequest();
+
+        Usuario usuario = new Usuario();
+
+        Mockito.when(mapper.map(request, Usuario.class)).thenReturn(usuario);
+        Mockito.when(repository.save(usuario)).thenReturn(usuario);
+
+        adicionarNovoUsuarioService.adicionar(request);
+
+        Assert.assertEquals(adicionarNovoUsuarioService.adicionar(request), usuario);
     }
 }
