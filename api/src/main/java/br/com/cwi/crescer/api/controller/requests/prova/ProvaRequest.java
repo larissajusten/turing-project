161deb13ed148d10886307c8291216b1b8d312c6
Larissa Espelocin Justen <larissa.justen@cwi.com.br>
2019-11-18 14:24:22 -0200

    update

--- api/src/main/java/br/com/cwi/crescer/api/controller/requests/prova/ProvaRequest.java
+++ api/src/main/java/br/com/cwi/crescer/api/controller/requests/prova/ProvaRequest.java
@@ -3,6 +3,7 @@ package br.com.cwi.crescer.api.controller.requests.prova;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -13,11 +14,10 @@ public class ProvaRequest {
     @NotEmpty (message = "O email do candidato não pode ser vazio")
     private String email;
 
-    @NotNull (message = "O tempo de duração da prova não pode ser vazio")
+    @Min(value = 0, message = "O tempo para iniciar a prova não pode ser vazio")
     private int tempoDeDuracaoDaProva;
 
-    @NotEmpty
-    @NotNull (message = "O tempo para iniciar a prova não pode ser vazio")
+    @Min(value = 0, message = "O tempo para iniciar a prova não pode ser vazio")
     private int tempoParaInicioProva;
 
 }
