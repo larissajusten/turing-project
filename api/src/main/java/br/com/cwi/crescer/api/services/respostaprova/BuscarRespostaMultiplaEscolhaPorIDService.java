8db682ff6c31b7e6fa6b84d8eb4247d173d9d482
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2019-11-18 11:40:08 -0300

    com validator pra nota e nova excessao pra nao achar resposta

---
+++ api/src/main/java/br/com/cwi/crescer/api/services/respostaprova/BuscarRespostaMultiplaEscolhaPorIDService.java
@@ -0,0 +1,21 @@
+package br.com.cwi.crescer.api.services.respostaprova;
+
+import br.com.cwi.crescer.api.domain.resposta.RespostasMultiplaEscolhaProva;
+import br.com.cwi.crescer.api.domain.resposta.RespostasTecnicaProva;
+import br.com.cwi.crescer.api.exception.resposta.RespostaNaoEncontradaException;
+import br.com.cwi.crescer.api.repository.resposta.RespostaMultiplaEscolhaRepository;
+import br.com.cwi.crescer.api.repository.resposta.RespostasTecnicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BuscarRespostaMultiplaEscolhaPorIDService {
+
+    @Autowired
+    private RespostaMultiplaEscolhaRepository repository;
+
+    public RespostasMultiplaEscolhaProva buscar(Long id){
+        return repository.findById(id).orElseThrow(() -> new
+                RespostaNaoEncontradaException("Resposta de múltipla escolha não encontrada por esse ID"));
+    }
+}
--- api/src/main/java/br/com/cwi/crescer/api/services/respostaprova/CorrigirQuestaoDissertativaService.java
+++ api/src/main/java/br/com/cwi/crescer/api/services/respostaprova/CorrigirQuestaoDissertativaService.java
@@ -4,19 +4,26 @@ import br.com.cwi.crescer.api.controller.requests.questoes.CorrecaoProvaRequest;
 import br.com.cwi.crescer.api.domain.resposta.RespostasDissertativaProva;
 import br.com.cwi.crescer.api.exception.ValidacaoDeAplicacaoException;
 import br.com.cwi.crescer.api.repository.resposta.RespostasDissertativaRepository;
+import br.com.cwi.crescer.api.validator.NotaValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CorrigirQuestaoDissertativaService {
 
-
     @Autowired
     private RespostasDissertativaRepository repository;
 
+    @Autowired
+    private BuscarRespostaDissertativaPorIDService buscarRespostaDissertativaPorIDService;
+
+    @Autowired
+    private NotaValidator notaValidator;
+
     public void corrigir(Long idResposta, CorrecaoProvaRequest correcao){
-        RespostasDissertativaProva resposta =  repository.findById(idResposta)
-                .orElseThrow(() -> new ValidacaoDeAplicacaoException("Não foi encontrada a resposta"));
+        RespostasDissertativaProva resposta =  buscarRespostaDissertativaPorIDService.buscar(idResposta);
+
+        notaValidator.verificarSeNotaEMaiorQue0EMenorQueDez(correcao.getNota());
 
         resposta.setNota(correcao.getNota());
         resposta.setComentario(correcao.getComentario());
