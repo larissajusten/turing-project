603db61aaf4582eb54a645557a503fbc77842d59
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2019-11-18 16:46:16 -0300

    endpoint para mostrar media de dissertativa e tecnica

---
+++ api/src/main/java/br/com/cwi/crescer/api/services/provaquestao/CalcularNotaMediaDissertativaService.java
@@ -0,0 +1,34 @@
+package br.com.cwi.crescer.api.services.provaquestao;
+
+import br.com.cwi.crescer.api.domain.questao.AlternativaMultiplaEscolha;
+import br.com.cwi.crescer.api.domain.resposta.RespostasDissertativaProva;
+import br.com.cwi.crescer.api.domain.resposta.RespostasMultiplaEscolhaProva;
+import br.com.cwi.crescer.api.repository.prova.ProvaQuestaoDissertativaRepository;
+import br.com.cwi.crescer.api.repository.questao.AlternativaMultiplaEscolhaRepository;
+import br.com.cwi.crescer.api.repository.resposta.RespostaMultiplaEscolhaRepository;
+import br.com.cwi.crescer.api.repository.resposta.RespostasDissertativaRepository;
+import br.com.cwi.crescer.api.services.alternativamultiplaescolha.BuscarAlternativaQuestaoMultiplaEscolhaService;
+import br.com.cwi.crescer.api.services.alternativamultiplaescolha.RetornarAlternativaCertaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CalcularNotaMediaDissertativaService {
+
+    @Autowired
+    private RespostasDissertativaRepository repository;
+
+    public double calcular(Long idProva) {
+        double media = 0;
+
+        List<RespostasDissertativaProva> listaRespostas = repository.findAllByProvaIdEquals(idProva);
+
+        for (RespostasDissertativaProva respostasDissertativaProva : listaRespostas) {
+            media += respostasDissertativaProva.getNota();
+        }
+
+        return media/listaRespostas.size();
+    }
+}
