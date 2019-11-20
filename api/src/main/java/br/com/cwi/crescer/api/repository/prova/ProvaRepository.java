25ee6ed406dfa1abad70aed8a0a723899011452e
Antonio Marcos dos Santos da Rosa <antonio.rosa@cwi.com.br>
2019-11-20 15:56:55 -0200

    Retorno paginado de provas para correcao

--- api/src/main/java/br/com/cwi/crescer/api/repository/prova/ProvaRepository.java
+++ api/src/main/java/br/com/cwi/crescer/api/repository/prova/ProvaRepository.java
@@ -2,6 +2,8 @@ package br.com.cwi.crescer.api.repository.prova;
 
 import br.com.cwi.crescer.api.domain.enums.StatusProva;
 import br.com.cwi.crescer.api.domain.prova.Prova;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProvaRepository extends JpaRepository<Prova, Long> {
@@ -9,5 +11,7 @@ public interface ProvaRepository extends JpaRepository<Prova, Long> {
     Prova findAllByCriadorIdEquals(Long id);
 
     boolean existsByEmailCandidatoEqualsAndStatusEquals(String email, StatusProva ativa);
+
+    Page<Prova> findAllByStatusEquals(Pageable pageable, StatusProva status);
 }
 
