9145ca65d732511e328b9b4b920a26eb4255d660
antonio.rosa <antonio.rosa@cwi.com.br>
2019-11-22 11:45:41 -0200

    primeiro passo do login

--- api/src/main/java/br/com/cwi/crescer/api/security/ResourceServerConfig.java
+++ api/src/main/java/br/com/cwi/crescer/api/security/ResourceServerConfig.java
@@ -1,33 +1,37 @@
-//package br.com.cwi.crescer.api.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//
-//@Configuration
-//@EnableResourceServer
-//@EnableOAuth2Client
-//public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors().and().csrf().disable()
-//                .exceptionHandling()
-//                .and()
-//
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//
-//                .antMatchers("/**")
-//                .permitAll()
-//
-//                .anyRequest()
-//                .authenticated();
-//    }
-//}
+package br.com.cwi.crescer.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableOAuth2Client
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .exceptionHandling()
+                .and()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
+
+                .anyRequest()
+                .authenticated();
+    }
+}
---
+++ api/src/main/java/br/com/cwi/crescer/api/services/login/LoginService.java
@@ -0,0 +1,38 @@
+package br.com.cwi.crescer.api.services.login;
+
+import br.com.cwi.crescer.api.config.RestTemplateConfig;
+import br.com.cwi.crescer.api.domain.login.AccessTokenDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private String getAccessToken(String login, String senha) {
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("username", login);
+        form.add("password", senha);
+        form.add("client_id", "clima-app");
+        form.add("client_secret", "clima-homologacao");
+        form.add("grant_type", "password");
+        form.add("scopes", "clima-api openid");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(form, header);
+        String url = String.format("%s/connect/token", "https://auth-homolog.cwi.com.br");
+        AccessTokenDto accessTokenDto = restTemplate.postForObject(url, request, AccessTokenDto.class);
+        return accessTokenDto.getAccesToken();
+    }
+
+}
