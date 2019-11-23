cbe0804502a258bb64be61836830af3598a8f4bb
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2019-11-23 12:55:06 -0300

    dissertativa e values das notas arrumados

--- api/src/main/java/br/com/cwi/crescer/api/mapper/ProvaMapper.java
+++ api/src/main/java/br/com/cwi/crescer/api/mapper/ProvaMapper.java
@@ -4,6 +4,8 @@ import br.com.cwi.crescer.api.controller.requests.prova.ProvaRequest;
 import br.com.cwi.crescer.api.domain.enums.StatusProva;
 import br.com.cwi.crescer.api.domain.prova.Prova;
 import br.com.cwi.crescer.api.domain.usuario.Usuario;
+import br.com.cwi.crescer.api.security.LoggedUser;
+import br.com.cwi.crescer.api.services.usuario.BuscarUsuarioPeloEmailService;
 import br.com.cwi.crescer.api.services.usuario.BuscarUsuarioPorIdService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +22,19 @@ public class ProvaMapper {
     @Autowired
     private BuscarUsuarioPorIdService buscarUsuarioPorIdService;
 
+    @Autowired
+    private BuscarUsuarioPeloEmailService buscarUsuarioPeloEmailService;
+
+    @Autowired
+    private LoggedUser loggedUser;
+
     public Prova transformar(ProvaRequest request) {
         Prova prova = mapper.map(request, Prova.class);
 
         prova.setDataCriacao(LocalDateTime.now());
         prova.setEmailCandidato(request.getEmail());
 
-        //TODO mudar quando tiver usuário
-        Usuario usuario = buscarUsuarioPorIdService.buscar(1L);
+        Usuario usuario = buscarUsuarioPeloEmailService.buscar(loggedUser.getLogin());
 
         prova.setCriador(usuario);
         prova.setStatus(StatusProva.ATIVA);
