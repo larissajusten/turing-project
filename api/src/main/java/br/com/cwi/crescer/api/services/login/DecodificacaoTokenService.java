4c76f5f310b5835946334a823bc8db53407d3e8d
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2019-11-25 12:07:07 -0300

    apontamentos sonar arrumado

--- api/src/main/java/br/com/cwi/crescer/api/services/login/DecodificacaoTokenService.java
+++ api/src/main/java/br/com/cwi/crescer/api/services/login/DecodificacaoTokenService.java
@@ -1,6 +1,7 @@
 package br.com.cwi.crescer.api.services.login;
 
 import br.com.cwi.crescer.api.domain.usuario.LoggedUserDTO;
+import br.com.cwi.crescer.api.exception.usuario.UsuarioNaoEncontradoException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ public class DecodificacaoTokenService {
                     .setNome(jsonNode
                             .get("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").asText());
         } catch (Exception e) {
-            e.printStackTrace();
+           throw new UsuarioNaoEncontradoException("Decodificação do token falhou");
         }
         return usuarioLogado;
     }
