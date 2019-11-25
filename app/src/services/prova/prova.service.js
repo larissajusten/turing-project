fb5b29b06c515b89583b04a6f28a98ed01837df0
Larissa Espelocin <larissa.justen@cwi.com.br>
2019-11-25 11:48:31 -0300

    complicou alguns  imports

--- app/src/services/prova/prova.service.js
+++ app/src/services/prova/prova.service.js
@@ -3,7 +3,6 @@ import { baseUrl } from '../baseUrl';
 
 let token = localStorage.getItem('accessToken');
 
-//adicionarProva
 export const criarProva = async (prova) => {
 	const response = await Axios.post(`${baseUrl}/prova`, prova, { headers: { Authorization: token } });
 	return response.data;
