3bfd11d34b646eaddda5e5c1eb2483fdbc2af2ff
VanessaSchenkel <v.s.schenkel@gmail.com>
2019-11-26 00:51:19 -0300

    NINGUEM MAIS DA MERGE

--- app/src/services/prova/incluirQuestoesProva.service.js
+++ app/src/services/prova/incluirQuestoesProva.service.js
@@ -1,5 +1,5 @@
-import Axios from 'axios'
-import { baseUrl } from '../baseUrl'
+import Axios from 'axios';
+import { baseUrl } from '../baseUrl';
 
 let token;
 
@@ -10,22 +10,25 @@ function carregarToken() {
 }
 
 export const incluirDissertativas = async (idProva, questao) => {
-  carregarToken();
-  const response = await Axios.put(`${baseUrl}/incluir/${idProva}/incluir-dissertativa`, questao,
-  { headers: { Authorization: token }})
-  return response.data
-}
+	carregarToken();
+	const response = await Axios.put(`${baseUrl}/incluir/${idProva}/dissertativa`, questao, {
+		headers: { Authorization: token }
+	});
+	return response.data;
+};
 
 export const incluirTecnicas = async (idProva, questao) => {
-  carregarToken();
-  const response = await Axios.put(`${baseUrl}/incluir/${idProva}/incluir-tecnica`, questao,
-  { headers: { Authorization: token }})
-  return response.data
-}
+	carregarToken();
+	const response = await Axios.put(`${baseUrl}/incluir/${idProva}/tecnica`, questao, {
+		headers: { Authorization: token }
+	});
+	return response.data;
+};
 
 export const incluirMultiplaEscolha = async (idProva, questao) => {
-  carregarToken();
-  const response = await Axios.put(`${baseUrl}/incluir/${idProva}/incluir-multipla-escolha`, questao,
-  { headers: { Authorization: token }})
-  return response.data
-}
+	carregarToken();
+	const response = await Axios.put(`${baseUrl}/incluir/${idProva}/multipla-escolha`, questao, {
+		headers: { Authorization: token }
+	});
+	return response.data;
+};
--- app/src/tela/TelaInicial/telaInicial.tela.jsx
+++ app/src/tela/TelaInicial/telaInicial.tela.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom'
-import { retornarEspecificidades,	
-					retornarResultadosMultipla,
-					retornarResultadosDissertativa,
-					retornarResultadosTecnica } from '../../services/index';
-import { Notificacao, Select, GraficoMultipla, GraficoNotaDissertativa, GraficoNotaTecnica } from '../../component/index';
+import { Redirect } from 'react-router-dom';
+import {
+	retornarEspecificidades,
+	retornarResultadosMultipla,
+	retornarResultadosDissertativa,
+	retornarResultadosTecnica
+} from '../../services/index';
+import {
+	Notificacao,
+	Select,
+	GraficoMultipla,
+	GraficoNotaDissertativa,
+	GraficoNotaTecnica
+} from '../../component/index';
 import './telaInicial.style.css';
 
 const mensagemSucessoNotificacao = 'Busca bem sucedida';
@@ -22,28 +30,28 @@ export class TelaInicialScreen extends Component {
 	}
 
 	async componentDidMount() {
-		let token = localStorage.getItem('accessToken');
-		if (!token) {
-			this.setState({
-				deveRenderizarLogin: true
-			});
-		}else{
-			this.setState({
-				especificidades: await retornarEspecificidades()
-			})
-		}
-	}	
+		//let token = localStorage.getItem('accessToken');
+		//if (!token) {
+		//	this.setState({
+		//			deveRenderizarLogin: true
+		//		});
+		//	}else{
+		this.setState({
+			especificidades: await retornarEspecificidades()
+		});
+		//	}
+	}
 
 	handleChange = async (event) => {
 		const { name, value } = event.target;
-		this.setState({ [name]: value }, async() => {
+		this.setState({ [name]: value }, async () => {
 			this.setState({
 				notasMultipla: await retornarResultadosMultipla(this.state.especificidadeEscolhida),
 				notasDissertativas: await retornarResultadosDissertativa(this.state.especificidadeEscolhida),
 				notasTecnicas: await retornarResultadosTecnica(this.state.especificidadeEscolhida)
-			})
-		})
-	}
+			});
+		});
+	};
 
 	catchErrorENotifica(error) {
 		if (error.response.data.errors) {
@@ -56,7 +64,7 @@ export class TelaInicialScreen extends Component {
 	}
 
 	salvaResponseENotificaSucesso(especificidades) {
-		this.setState({ especificidades })
+		this.setState({ especificidades });
 		Notificacao('Sucesso', mensagemSucessoNotificacao, 'success');
 	}
 
@@ -64,86 +72,81 @@ export class TelaInicialScreen extends Component {
 		if (this.state.especificidades) {
 			return (
 				<div className="container-select">
-					<h1 className="subtitulo-especificidade">Para qual especificidade deseja visualizar as estatisticas?</h1>
+					<h1 className="subtitulo-especificidade">
+						Para qual especificidade deseja visualizar as estatisticas?
+					</h1>
 					<div className="input-principal">
-					<Select
-						questoesWidth="width-select"
-						name="especificidadeEscolhida"
-						value={this.state.especificidadeEscolhida}
-						onChange={this.handleChange}
-						object={this.state.especificidades}
-						placeholder="Selecione a especificidade"
-					/>
+						<Select
+							questoesWidth="width-select"
+							name="especificidadeEscolhida"
+							value={this.state.especificidadeEscolhida}
+							onChange={this.handleChange}
+							object={this.state.especificidades}
+							placeholder="Selecione a especificidade"
+						/>
+					</div>
 				</div>
-			</div>
 			);
-		}
-		else {
-			return <span className="titulo-crie"> Não há especificidades para gerar gráficos </span>
+		} else {
+			return <span className="titulo-crie"> Não há especificidades para gerar gráficos </span>;
 		}
 	}
 
 	renderMultiplasEscolhas() {
-		if(this.state.notasMultipla.length > 0){
-			return <GraficoMultipla notas={this.state.notasMultipla}/>
-		}else{
-			return <h1 className="titulo-crie">Não há questões</h1>
+		if (this.state.notasMultipla.length > 0) {
+			return <GraficoMultipla notas={this.state.notasMultipla} />;
+		} else {
+			return <h1 className="titulo-crie">Não há questões</h1>;
 		}
 	}
 
 	renderDissertativas() {
-		if(this.state.notasDissertativas.length > 0){
-			return <GraficoNotaDissertativa notas={this.state.notasDissertativas}/>
-		}else{
-			return <h1 className="titulo-crie">Não há questões</h1>
+		if (this.state.notasDissertativas.length > 0) {
+			return <GraficoNotaDissertativa notas={this.state.notasDissertativas} />;
+		} else {
+			return <h1 className="titulo-crie">Não há questões</h1>;
 		}
 	}
 
 	renderTecnicas() {
-		if(this.state.notasTecnicas.length > 0){
-			return <GraficoNotaTecnica notas={this.state.notasTecnicas}/>
-		}else{
-			return <h1 className="titulo-crie">Não há questões</h1>
+		if (this.state.notasTecnicas.length > 0) {
+			return <GraficoNotaTecnica notas={this.state.notasTecnicas} />;
+		} else {
+			return <h1 className="titulo-crie">Não há questões</h1>;
 		}
 	}
 
 	renderGraficos() {
-		return(
-			<>
-			<div className="container-nivel">
-				<h6 className="nivel facil">Fácil</h6>
-				<h6 className="nivel medio">Medio</h6>
-				<h6 className="nivel dificil">Difícil</h6>
+		return (
+			<div>
+				<div className="container-nivel">
+					<h6 className="nivel facil">Fácil</h6>
+					<h6 className="nivel medio">Medio</h6>
+					<h6 className="nivel dificil">Difícil</h6>
+				</div>
+				<h1 className="titulo-crie">Multiplas escolhas</h1>
+				<div className="graficos">{this.renderMultiplasEscolhas()}</div>
+				<h1 className="titulo-crie">Dissertativas</h1>
+				<div className="graficos">{this.renderDissertativas()}</div>
+				<h1 className="titulo-crie">Técnicas</h1>
+				<div className="graficos">{this.renderTecnicas()}</div>
 			</div>
-			<h1 className="titulo-crie">Multiplas escolhas</h1>
-			<div className="graficos">
-				{this.renderMultiplasEscolhas()}
-			</div>
-			<h1 className="titulo-crie">Dissertativas</h1>
-			<div className="graficos">
-				{this.renderDissertativas()}
-			</div>
-			<h1 className="titulo-crie">Técnicas</h1>
-			<div className="graficos">
-				{this.renderTecnicas()}
-			</div>
-			</>
-		)
+		);
 	}
 
 	render() {
 		if (this.state.deveRenderizarLogin) {
 			return <Redirect to="/login" />;
 		}
-    return (
-      <>
-			<div className="container-tela">
-				<h1 className="titulo">Dashboard</h1>
-				{this.renderContainerComponent()}
-				
-				{this.state.especificidadeEscolhida && this.renderGraficos()}
-      </div>
-      </>
+		return (
+			<div>
+				<div className="container-tela">
+					<h1 className="titulo">Dashboard</h1>
+					{this.renderContainerComponent()}
+
+					{this.state.especificidadeEscolhida && this.renderGraficos()}
+				</div>
+			</div>
 		);
 	}
 }
