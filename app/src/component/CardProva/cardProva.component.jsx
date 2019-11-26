1e1eba023db62ab0918da14a40c7790be224d2e5
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2019-11-25 21:45:04 -0300

    mais dados

--- app/src/component/CardProva/cardProva.component.jsx
+++ app/src/component/CardProva/cardProva.component.jsx
@@ -7,9 +7,9 @@ export class CardProva extends Component {
     return (
       <div className="container-card" onClick={(event) => this.props.onClick(event, this.props.id)}>
         <div className="content-card">
-          <span>{this.props.informacaoUm}</span>
-          <span>{this.props.informacaoDois}</span>
-          <span>{this.props.informacaoTres}</span>
+          <span>{`Nome do candidato: ${this.props.informacaoUm}`}</span>
+          <span>{`E-mail do candidato: ${this.props.informacaoDois}`}</span>
+          <span>{`Duração da prova: ${this.props.informacaoTres}`}</span>
         </div>
       </div>
     )
