d70057647d1c6762e1bdd04e43f2e54f4ded1727
Larissa Espelocin Justen <larissa.justen@cwi.com.br>
2019-11-27 12:49:24 -0200

    Botao de corrigir no card da prova

--- app/src/component/CardProva/cardProva.component.jsx
+++ app/src/component/CardProva/cardProva.component.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import './cardProva.style.css'
+import { BotaoPrincipal } from '../index'
 
 export class CardProva extends Component {
 
   render() {
     return (
-      <div className="container-card" onClick={(event) => this.props.onClick(event, this.props.id)}>
+      <div className="container-card">
         <div className="content-card">
-          <span>{`Nome do candidato: ${this.props.informacaoUm}`}</span>
-          <span>{`E-mail do candidato: ${this.props.informacaoDois}`}</span>
-          <span>{`Duração da prova: ${this.props.informacaoTres}`}</span>
+          <div>
+            <span>Nome do candidato:</span>
+            <span>{this.props.informacaoUm}</span>
+          </div>
+          <div>
+            <span>E-mail do candidato:</span>
+            <span>{this.props.informacaoDois}</span>
+          </div>
+          <div>
+            <span>Duração:</span>
+            <span>{this.props.informacaoTres}</span>
+          </div>
         </div>
+        <BotaoPrincipal classe="tamanho-botao" nome="CORRIGIR" onClick={(event) => this.props.onClick(event, this.props.id)}/>
       </div>
     )
   }
