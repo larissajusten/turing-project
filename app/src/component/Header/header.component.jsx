5c93191503ff450160b626d14ded9efe3ea3aca7
Guilherme Gabriel Rigotti <guilherme.rigotti@cwi.com.br>
2019-12-13 14:16:34 -0300

    Fix menu bug

--- app/src/component/Header/header.component.jsx
+++ app/src/component/Header/header.component.jsx
@@ -4,19 +4,55 @@ import { Link } from 'react-router-dom'
 import './header.style.css'
 
 export class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuAberto: false
+    }
+  }
+
   render() {
     return (
       <div className="header">
-        <span className="menu-header" onClick={this.handleClickShowMenu}>
+        <span
+          className="menu-header"
+          onClick={() => this.setState({ menuAberto: true })}
+        >
           MENU
         </span>
-        <Menu width={'250px'}>
+
+        <Menu isOpen={this.state.menuAberto} width={'250px'}>
           <Link to={'/'}>Home</Link>
-          <Link to={'/cadastrar-questao'}>Cadastrar questão</Link>
-          <Link to={'/buscar-questao'}>Visualizar questões</Link>
-          <Link to={'/cadastrar-prova'}>Cadastrar prova</Link>
-          <Link to={'/buscar-provas'}>Busca para correção</Link>
-          <Link to={'/provas-corrigidas'}>Provas corrigidas</Link>
+          <Link
+            onClick={() => this.setState({ menuAberto: false })}
+            to={'/cadastrar-questao'}
+          >
+            Cadastrar questão
+          </Link>
+          <Link
+            onClick={() => this.setState({ menuAberto: false })}
+            to={'/buscar-questao'}
+          >
+            Visualizar questões
+          </Link>
+          <Link
+            onClick={() => this.setState({ menuAberto: false })}
+            to={'/cadastrar-prova'}
+          >
+            Cadastrar prova
+          </Link>
+          <Link
+            onClick={() => this.setState({ menuAberto: false })}
+            to={'/buscar-provas'}
+          >
+            Busca para correção
+          </Link>
+          <Link
+            onClick={() => this.setState({ menuAberto: false })}
+            to={'/provas-corrigidas'}
+          >
+            Provas corrigidas
+          </Link>
         </Menu>
       </div>
     )
