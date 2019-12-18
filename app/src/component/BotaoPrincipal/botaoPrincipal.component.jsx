5b1eccd957b6ed62812aea2c1134525916525728
Vanessa Schramm Schenkel da Silva <vanessa.silva@cwi.com.br>
2019-12-18 13:51:05 -0300

    botao

--- app/src/component/BotaoPrincipal/botaoPrincipal.component.jsx
+++ app/src/component/BotaoPrincipal/botaoPrincipal.component.jsx
@@ -7,8 +7,8 @@ export class BotaoPrincipal extends Component {
       <>
         <button
           className={`botao-principal ${this.props.classe}`}
-          onClick={() =>
-            this.props.onClick(this.props.nomeUsuario, this.props.emailUsuario)
+          onClick={(event) =>
+            this.props.onClick(event,this.props.nomeUsuario, this.props.emailUsuario)
           }
         >
           <span>{this.props.nome}</span>
